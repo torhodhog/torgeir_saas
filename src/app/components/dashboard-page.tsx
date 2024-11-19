355936c0c0ab3fa565fe5639ca7c910268f5c0cb
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-11-19 21:04:29 +0100

    Beautiful dashboard page - with category-page-content ok.

--- src/app/components/dashboard-page.tsx
+++ src/app/components/dashboard-page.tsx
@@ -1,7 +1,10 @@
+
+'use client'
 import { ReactNode } from "react"
 import { Button } from "./ui/button"
 import { ArrowLeft } from "lucide-react"
 import { Heading } from "./Heading"
+import { useRouter } from "next/navigation"
 
 interface DashboardPageProps {
   title: string
@@ -16,13 +19,15 @@ export const DashboardPage = ({
   hideBackBtton,
   cta,
 }: DashboardPageProps) => {
+
+  const router = useRouter()
   return (
     <section className="flex-1 h-full w-full flex flex-col">
       <div className="w-full p-6 sm:p-8 flex justify-between border-b border-gray-200">
         <div className="w-full flex flex-col sm:flew-row items-start gap-y-6">
           <div className="flex items-center gap-8">
           {hideBackBtton ? null : (
-            <Button className="w-fit bg-white" variant="outline">
+            <Button onClick={() => router.push("/dashboard")} className="w-fit bg-white" variant="outline">
               <ArrowLeft className="size-4" />
             </Button>
           )}
